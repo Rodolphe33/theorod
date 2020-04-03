329856dfa273385d4a7754e46a48b4c0b8b9cbd1
NDWebmaster <56785059+dusseautnicolas@users.noreply.github.com>
2020-04-03 12:27:36 +0200

    des commentaires
    quelques commentaires

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // hide
 $(document).ready(function() {
-    $('#id1').click(function() {
-        $("#cache").slideToggle();
+    
+    // voilà, ici c'est le déclencheur, l'élément HTML sur lequel tu appliques un event, donc un clic de souris
+    $('#id1').click(function() { // attention à l'ID entre parenthèse, il doit s'agir d'un div ou une balise titre SEUL, sans enfant.
+       
+        // ici c'est la méthode que tu appliques sur ton event (un clic de souris déclenche un menu toggle)
+        $("#cache").slideToggle(); // ton ID cache doit être une balise parente, elle contient des balises enfants avec les contenus de ton intervention
+        
     });
 });
 
@@ -15,23 +20,23 @@ $(document).ready(function() {
 // show
 $(document).ready(function() {
     $('#id1').click(function() {
-        $('.hide').slideToggle(1000);
+        $('.hide').slideToggle(1000); // ici, tu n'as pas besoin du 1000, le slideToggle génère déjà une fluidité dans l'animation =)
     });
 });
 $(document).ready(function() {
     $('#line2').click(function() {
-        $('').hide(1000);
+        $('').hide(1000); // ici tu n'as pas d'élément déclaré dans ta variable $... donc il n'y a rien à cacher
     });
 });
 $(document).ready(function() {
     $('#line3').click(function() {
-        $('').hide(1000);
+        $('').hide(1000); // ici tu n'as pas d'élément déclaré dans ta variable $... donc il n'y a rien à cacher
     });
 });
 
 $(document).ready(function() {
     $('#line4').click(function() {
-        $('').show(1000);
+        $('').show(1000); // ici tu n'as pas d'élément déclaré dans ta variable $... donc il n'y a rien à cacher
     });
 });
 
